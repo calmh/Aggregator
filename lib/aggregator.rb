7afc975f3f411d73e6958d1f7e0e0a0c8f2da446
Jakob Borg <jakob@nym.se>
2010-01-11 09:33:31 +0100

    Separate core extension as per best practices.

--- lib/aggregator.rb
+++ lib/aggregator.rb
@@ -1,29 +1,6 @@
+require 'extensions'
 require 'mysql'
 require 'date'
-
-# Define some convenient shorthands for specifying times
-class Numeric
-	def minute ; self * 60 ; end
-	def hour ; self * 3600 ; end
-	def day ; self * 86400 ; end
-	def week ; 7 * day ; end
-	def month ; 30 * day ; end
-	def year ; 365 * day ; end
-	def ago ; (Time.new.to_f - self).to_i ; end
-	def minutes ; minute ; end
-	def hours ; hour ; end
-	def days ; day ; end
-	def weeks ; week ; end
-	def months ; month ; end
-	def years ; year ; end
-end
-
-class String
-	# Match string or regexp
-	def apprmatch(obj)
-		obj.kind_of?(String) && self == obj || obj.kind_of?(Regexp) && self =~ obj
-	end
-end
 
 class Aggregator
 	# Rules for handling tables. Add hashes of that include the keys:
